59e21e2cdafae60273fb89b01a66b90ac6b814d9
tahreemareesha <tahreemareesha@gmail.com>
2026-07-20 11:51:30 +0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var btn = document.getElementById('navtoggle');
-  var links = document.getElementById('navlinks');
-  if (btn && links) {
-    btn.addEventListener('click', function () {
-      links.classList.toggle('open');
-    });
-  }
-});
